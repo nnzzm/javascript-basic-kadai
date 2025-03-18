4a69f62c102ef1a87e0e5a9cbf45d3d54c92b3d1
nnzzm <nzmtomjerry1245@docomo.ne.jp>
2025-03-18 19:22:00 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+let time=new Date();
+// console.log(time); 
+
+let year=time.getFullYear();
+let month=time.getMonth()+1;
+let date=time.getDate();
+
+console.log(year+"年"+month+"月"+date+"日"); // 2021年5月1日
